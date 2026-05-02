6f31039cde22f4592be96406a234fc72846a8f31
Wmande <mandelanelson194@gmail.com>
2026-05-02 17:26:43 +0300

    deploying finally

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,12 @@ Route::get('/news', [NewsController::class, 'index']);
 Route::post('/news', [NewsController::class, 'store']);
 Route::put('/news/{id}', [NewsController::class, 'update']);
 Route::delete('/news/{id}', [NewsController::class, 'destroy']);
+
+Route::get('/debug', function () {
+    return response()->json([
+        'supabase_url' => config('services.supabase.url'),
+        'supabase_key_set' => !empty(config('services.supabase.service_role_key')),
+        'pgsql_loaded' => extension_loaded('pdo_pgsql'),
+        'php_version' => PHP_VERSION,
+    ]);
+});
