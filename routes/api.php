2eeaf4a5a1b62cf690564637dd46c75e9ab5bfc6
Wmande <mandelanelson194@gmail.com>
2026-05-02 17:31:56 +0300

    deploying finally

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,10 +10,17 @@ Route::post('/news', [NewsController::class, 'store']);
 Route::put('/news/{id}', [NewsController::class, 'update']);
 Route::delete('/news/{id}', [NewsController::class, 'destroy']);
 
+Route::get('/parish/info', function () {
+    return response()->json([
+        'name' => 'St. Leo The Great',
+        'diocese' => 'Diocese of Bungoma',
+    ]);
+});
+
 Route::get('/debug', function () {
     return response()->json([
         'supabase_url' => config('services.supabase.url'),
-        'supabase_key_set' => !empty(config('services.supabase.service_role_key')),
+        'supabase_key_set' => ! empty(config('services.supabase.service_role_key')),
         'pgsql_loaded' => extension_loaded('pdo_pgsql'),
         'php_version' => PHP_VERSION,
     ]);
